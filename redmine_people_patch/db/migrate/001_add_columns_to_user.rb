767733a364ab807ec604973d15a95329dd6fc56a
bilel-kedidi <bilel.kedidi@gmail.com>
2016-01-15 16:30:18 +0100

    patch

---
+++ redmine_people_patch/db/migrate/001_add_columns_to_user.rb
@@ -0,0 +1,12 @@
+
+class AddColumnsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :client_type, :string
+    add_column :users, :emp_id, :string
+    add_column :users, :personal_email_id, :string
+    add_column :users, :anual_ctc, :string
+    add_column :users, :experience, :string
+
+  end
+
+end
