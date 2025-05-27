0f3d30ea88a5bc194fa714254308cb8c2634381c
arean82 <arean82@gmail.com>
2025-05-27 14:43:00 +0530

    d

--- lib/redmine_people/patches/user_patch.rb
+++ lib/redmine_people/patches/user_patch.rb
@@ -1,30 +1,34 @@
-
-
 require_dependency 'redmine/acts/attachable_global'
-require_dependency 'project'
-require_dependency 'principal'
-require_dependency 'user'
 
 module RedminePeople
   module Patches
-
     module UserPatch
-      def self.included(base) # :nodoc:
+      def self.included(base)
         base.send(:include, InstanceMethods)
+
+        # ✅ Include AttachableGlobal before class_eval so `acts_as_attachable_global` becomes available
+        base.send(:include, Redmine::Acts::AttachableGlobal)
 
         base.class_eval do
           unloadable
-          if ActiveRecord::VERSION::MAJOR >= 4
-            has_one :avatar, lambda { where("#{Attachment.table_name}.description = 'avatar'")}, :class_name => "Attachment", :as  => :container, :dependent => :destroy
-          else
-            has_one :avatar, :class_name => "Attachment", :as  => :container, :conditions => "#{Attachment.table_name}.description = 'avatar'", :dependent => :destroy
-          end
+
+          # ✅ This will now work
           acts_as_attachable_global
+
+          if ActiveRecord::VERSION::MAJOR >= 4
+            has_one :avatar, lambda {
+              where("#{Attachment.table_name}.description = 'avatar'")
+            }, class_name: 'Attachment', as: :container, dependent: :destroy
+          else
+            has_one :avatar, class_name: 'Attachment', as: :container,
+                    conditions: "#{Attachment.table_name}.description = 'avatar'",
+                    dependent: :destroy
+          end
 
           def self.clear_safe_attributes
             @safe_attributes.collect! do |attrs, options|
               if attrs.collect!(&:to_s).include?('firstname') 
-                [attrs - ['firstname', 'lastname', 'mail', 'custom_field_values', 'custom_fields'] , options]
+                [attrs - ['firstname', 'lastname', 'mail', 'custom_field_values', 'custom_fields'], options]
               else
                 [attrs, options]
               end
@@ -33,13 +37,13 @@ module RedminePeople
           self.clear_safe_attributes
 
           safe_attributes 'firstname', 'lastname', 'mail', 'custom_field_values', 'custom_fields',
-          :if => lambda {|user, current_user| current_user.allowed_people_to?(:edit_people, user) }
+                          if: ->(user, current_user) {
+                            current_user.allowed_people_to?(:edit_people, user)
+                          }
         end
       end
 
       module InstanceMethods
-        # include ContactsHelper
-
         def project
           @project ||= Project.new
         end
@@ -47,30 +51,21 @@ module RedminePeople
         def allowed_people_to?(permission, person = nil)
           return true if admin?
 
-          if person && person.is_a?(User) && person.id == self.id
-            if :view_people == permission
-              return true
-            end
-
-            if :edit_people == permission && Setting.plugin_redmine_people['edit_own_data'].to_i > 0
-              return true
-            end
+          if person.is_a?(User) && person.id == id
+            return true if permission == :view_people
+            return true if permission == :edit_people && Setting.plugin_redmine_people['edit_own_data'].to_i > 0
           end
 
           return false unless RedminePeople.available_permissions.include?(permission)
-          return true if permission == :view_people && self.is_a?(User) && !self.anonymous? && Setting.plugin_redmine_people["visibility"].to_i > 0
+          return true if permission == :view_people && !anonymous? && Setting.plugin_redmine_people['visibility'].to_i > 0
 
-          (self.groups + [self]).map{|principal| PeopleAcl.allowed_to?(principal, permission) }.inject{|memo,allowed| memo || allowed }
+          (groups + [self]).any? { |principal| PeopleAcl.allowed_to?(principal, permission) }
         end
-
       end
     end
-
   end
 end
 
 unless User.included_modules.include?(RedminePeople::Patches::UserPatch)
   User.send(:include, RedminePeople::Patches::UserPatch)
 end
-
-
